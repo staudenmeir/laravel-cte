9981dfef2498e6e80c91dffa5fb80d79b9ca5ab0
Mohammad <usermp76@gmail.com>
2023-11-11 10:48:20 +0330

    Refactor ConnectionFactory to Use Match Expression

--- src/Connectors/ConnectionFactory.php
+++ src/Connectors/ConnectionFactory.php
@@ -22,31 +22,24 @@ class ConnectionFactory extends Base
      * @param string $database
      * @param string $prefix
      * @param array $config
-     * @return \Illuminate\Database\Connection
+     * @return MySqlConnection|OracleConnection|PostgresConnection|SingleStoreConnection|SQLiteConnection|SqlServerConnection
      *
      * @throws \InvalidArgumentException
      */
-    protected function createConnection($driver, $connection, $database, $prefix = '', array $config = [])
+    protected function createConnection(string $driver, \PDO|\Closure $connection, string $database, string $prefix = '', array $config = []): SQLiteConnection|SingleStoreConnection|PostgresConnection|MySqlConnection|OracleConnection|SqlServerConnection
     {
         if ($driver !== 'singlestore' && $resolver = Connection::getResolver($driver)) {
             return $resolver($connection, $database, $prefix, $config); // @codeCoverageIgnore
         }
 
-        switch ($driver) {
-            case 'mysql':
-                return new MySqlConnection($connection, $database, $prefix, $config);
-            case 'pgsql':
-                return new PostgresConnection($connection, $database, $prefix, $config);
-            case 'sqlite':
-                return new SQLiteConnection($connection, $database, $prefix, $config);
-            case 'sqlsrv':
-                return new SqlServerConnection($connection, $database, $prefix, $config);
-            case 'oracle':
-                return new OracleConnection($connection, $database, $prefix, $config); // @codeCoverageIgnore
-            case 'singlestore':
-                return new SingleStoreConnection($connection, $database, $prefix, $config);
-        }
-
-        throw new InvalidArgumentException("Unsupported driver [{$driver}]"); // @codeCoverageIgnore
+        return match ($driver) {
+            'mysql' => new MySqlConnection($connection, $database, $prefix, $config),
+            'pgsql' => new PostgresConnection($connection, $database, $prefix, $config),
+            'sqlite' => new SQLiteConnection($connection, $database, $prefix, $config),
+            'sqlsrv' => new SqlServerConnection($connection, $database, $prefix, $config),
+            'oracle' => new OracleConnection($connection, $database, $prefix, $config), // @codeCoverageIgnore
+            'singlestore' => new SingleStoreConnection($connection, $database, $prefix, $config),
+            default => throw new InvalidArgumentException("Unsupported driver [{$driver}]"), // @codeCoverageIgnore
+        };
     }
 }
